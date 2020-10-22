af9cf0987bf495c5e5cbcbbaeab91f812d0ecfb9
lindsaypenkrat <penkrat@bc.edu>
2020-10-21 22:19:30 -0400

    Bars displaying for stores

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-let data  = d3.csv('coffee-house-chains.csv', d3.autoType).then(data=>{
-    console.log(data);
-
-
     const margin = ({top: 50, right: 140, bottom: 20, left: 140});
     const width = 800 - margin.left - margin.right;
     const height = 550 - margin.top - margin.bottom;
+    let type = d3.select("#group-by").node().value;
 
-    let plot = d3.select('.chart').append('svg')
+    const plot = d3.select('.chart').append('svg')
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
         .append("g")
@@ -15,26 +12,13 @@ let data  = d3.csv('coffee-house-chains.csv', d3.autoType).then(data=>{
 
     const xScale = d3
         .scaleBand()
-        .domain(data.map(d => d.company))
         .rangeRound([0,width]) 
         .paddingInner(0.1)
 
 
     const yScale = d3
         .scaleLinear()
-        .domain([d3.min(data, d => d.stores) , d3.max(data, d => d.stores)])
         .range([height,0]);
-
-    plot.selectAll('rect')
-        .data(data)
-        .enter()
-        .append('rect')
-        .attr('class', 'buildings')
-        .attr('height',10) //dynamic element, change
-        .attr('width', xScale.bandwidth())
-        .attr('x', d=>xScale(d.company))
-        .attr('y', 10) //dynamic element, change
-        .attr('fill', 'skyblue') 
 
     const xAxis = d3.axisBottom()
         .scale(xScale)
@@ -55,6 +39,7 @@ let data  = d3.csv('coffee-house-chains.csv', d3.autoType).then(data=>{
 
 
     plot.append("text")
+        .attr("class", "y-axis-title")
         .attr('x', -60)
         .attr('y', -10)
         .attr("font-family", "sans-serif")
@@ -62,4 +47,29 @@ let data  = d3.csv('coffee-house-chains.csv', d3.autoType).then(data=>{
         .attr("fill", "#F3F3F3")
         .attr('text-anchor', 'start')
         .text("Stores");
-    }); 
+
+    function update(data,type){
+        // update domains
+        xScale.domain(data.map(d=>d.company))
+        yScale.domain([0, d3.max(data,d=>d[type])]);
+    
+        // update bars
+        let bars = plot.selectAll('rect')
+            .data(data, d => {return d.company; })
+            .enter()
+            .append('rect')
+            .attr('class', 'barsGraphed')
+            .attr('height',d => (height - yScale(d[type]))) 
+            .attr('width', xScale.bandwidth())
+            .attr('x', d=>xScale(d.company))
+            .attr('y', d=>yScale(d[type])) 
+            .attr('fill', '#FFFFFF');
+    
+        // update axes and axis title
+    
+    }
+
+    let data  = d3.csv('coffee-house-chains.csv', d3.autoType).then(data=>{
+        update(data,type); // simply call the update function with the supplied data
+
+    });
